81d4da5f857e525c79811d81603b8833e48c91eb
Cilez <kobrelislegryi@gmail.com>
2022-10-15 11:12:58 +0300

    Исправил все замечания. Поля с запросами закомментировал и пока оставил.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,38 +2,48 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
-
-        dao.createUsersTable();
-        dao.saveUser("Sergey", "Ivanko", (byte)(33));
-        dao.saveUser("Egor", "Foo", (byte)(44));
-        dao.saveUser("Andrey", "Fooo", (byte)(55));
-        dao.saveUser("Anton", "Som", (byte)(66));
-        for (User u : dao.getAllUsers()) {
-            System.out.println(u.toString());
-        }
-        dao.cleanUsersTable();
-        dao.dropUsersTable();
-
-//        Session sess = Util.getSessionFactory().openSession();
+//        UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
 //
-//        String sql2 = "Create table USERS(id serial, firstName varchar(100) NOT NULL, lastName varchar(100) NOT NULL, age INT NOT NULL, PRIMARY KEY (ID))";
-//        Transaction tx = null;
-//        try {
-//            tx = sess.beginTransaction();
-//            Query query = sess.createSQLQuery(sql2);
-//            tx.commit();
+//        dao.createUsersTable();
+//        dao.saveUser("Sergey", "Ivanko", (byte)(33));
+//        dao.saveUser("Egor", "Foo", (byte)(44));
+//        dao.saveUser("Andrey", "Fooo", (byte)(55));
+//        dao.saveUser("Anton", "Som", (byte)(66));
+//        for (User u : dao.getAllUsers()) {
+//            System.out.println(u.toString());
 //        }
-//        catch (Exception e) {
-//            if (tx!=null) tx.rollback();
-//            throw e;
-//        }
-//        finally {
-//            sess.close();
-//        }
+//        dao.cleanUsersTable();
+//        dao.dropUsersTable();
+
+        SessionFactory sessionFactory = Util.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        String sql = "CREATE TABLE IF NOT EXISTS users " +
+                "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
+                "age TINYINT NOT NULL)";
+
+        Query query = session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
+                "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "name VARCHAR(50) NOT NULL," +
+                "lastName VARCHAR(50) NOT NULL, " +
+                "age TINYINT NOT NULL)");
+
+//        String sql = "DROP TABLE IF EXISTS users";
+//
+//        Query query = session.createSQLQuery(sql);
+
+        session.getTransaction().commit();
+        sessionFactory.close();
+
 
 
     }
